444267d6262670ce8ac74c11769a41b8ec01c585
jwu8475 <jwu8475@gmail.com>
2023-10-28 14:16:26 -0500

    worked on cluster page and homepage page

--- src/component/Cluster.jsx
+++ src/component/Cluster.jsx
@@ -1,18 +1,53 @@
+import { useState } from "react"
+import { useNavigate } from "react-router"
+import { Link } from "react-router-dom"
+
 export default function Cluster() {
+    const [cluster, setCluster] = useState('')
+    const [isDisabled, setIsDisabled] = useState(false)
+    const navigate = useNavigate()
+
+    const clusterEndPoint = `/`
+    const submitCluster = async () => {
+        const res = await fetch(clusterEndPoint, {
+            method: 'POST',
+            body: JSON.stringify({
+                cluster: cluster,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        if (!res.ok) {
+            throw new Error('failed to fetch at cluster')
+        }
+
+        if (res.status === 200) navigate('/dashboard')
+    }
+
     return (
-        <>
-        <form>
+        <main className='p-8 bg-slate-300 border-slate-500 border-2 rounded-lg'>
+        <form className='space-y-3'>
+            <label>ENTER YOUR CLUSTER <br /></label>
             <input
             type='text'
             name='cluster'
-            placeholder='ENTER YOUR CLUSTER'
+            placeholder='ex: http://localhost:9090'
+            onSubmit={submitCluster}
+            onChange={(e) => {setCluster(e.target.value)}}
             />
             <br />
-            <button>
+            <button
+            className='border-slate-500 border-1 rounded-lg bg-slate-200'
+            name='enterCluster'
+            type='submit'
+            disabled={isDisabled}
+            onClick={() => setIsDisabled(true)}
+            >
                 ENTER
             </button>
         </form>
-
-        </>
+        <Link to="/dashboard">to dashboard</Link>
+        </main>
     )
 }
--- src/component/Homepage.jsx
+++ src/component/Homepage.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 export default function Homepage() {
+    const [auth, setAuth] = useState(true)
+
+
     return (
-        <main>
-        <nav>
+        <main className='fixed inset-0 flex flex-col bg-slate-300 border-slate-500 border-2 rounded-lg'>
+        <nav className="bg-white p-4 flex space-x-4 rounded-lg my-2 justify-between">
             <Link to='/dashboard/demo'>DEMO</Link>
             <Link to='/login'>Login</Link>
         </nav>
-        <Link to='/cluster'>enter your cluster</Link>
+        <div className="bg-white flex-grow flex-col flex items-center justify-center rounded-lg">
+            <p>Welcome message here</p>
+            <Link to='/dashboard'>DASHBOARD</Link>
+        </div>
         </main>
     )
 }
