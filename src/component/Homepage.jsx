88d93f6c59b24e4e6dbc14597de716b41cf8d578
jwu8475 <jwu8475@gmail.com>
2023-10-28 14:32:52 -0500

    updated homepage merge conflict

--- src/component/Homepage.jsx
+++ src/component/Homepage.jsx
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
 
 export default function Homepage() {
     const [auth, setAuth] = useState(true)
 
-
-    return (
-        <main className='fixed inset-0 flex flex-col bg-slate-300 border-slate-500 border-2 rounded-lg'>
-        <nav className="bg-white p-4 flex space-x-4 rounded-lg my-2 justify-between">
-            <Link to='/dashboard/demo'>DEMO</Link>
-=======
-import React, { useEffect } from 'react';
-import { Link, useNavigate } from "react-router-dom"
-
-export default function Homepage() {
     const navigate = useNavigate()
     useEffect(() => {
         async function verifyToken() {
@@ -25,11 +14,11 @@ export default function Homepage() {
         }
         verifyToken();
       }, []);
+
     return (
-        <main>
-        <nav>
-            <Link to='/dashboard'>Dashboard</Link>
->>>>>>> 0277970d55b4bf951c781adc67d2ee582bd1d827
+        <main className='fixed inset-0 flex flex-col bg-slate-300 border-slate-500 border-2 rounded-lg'>
+        <nav className="bg-white p-4 flex space-x-4 rounded-lg my-2 justify-between">
+            <Link to='/dashboard/demo'>DEMO</Link>
             <Link to='/login'>Login</Link>
         </nav>
         <div className="bg-white flex-grow flex-col flex items-center justify-center rounded-lg">
