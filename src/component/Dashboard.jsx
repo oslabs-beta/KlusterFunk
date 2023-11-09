b0676a1b2669c0ebe72cea068e9faf3acbbca4d0
Dominic Kenny <dominicjkenny@gmail.com>
2023-11-09 15:16:37 -0500

    Graphies are workin
    Co-authored-by: dtezz <dtezz@users.noreply.github.com>
    Co-authored-by: jwu8475 <jwu8475@users.noreply.github.com>
    Co-authored-by: conniedonahue <conniedonahue@users.noreply.github.com>

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,71 +1,139 @@
 import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import LineGraph from './LineGraph'
+import LineGraph from './LineGraph';
 
 export default function Dashboard() {
   const [metricStore, setMetricStore] = useState({
-    bytesIn: 'a',
-    bytesOut: 'a',
-    cpuUsage: 'a',
-    brokerCount: 'a',
-  })
+    bytesIn: [
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+    ],
+    bytesOut: [
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+    ],
+    cpuUsage: [
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+    ],
+    brokerCount: [
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+      ['-', '-'],
+    ],
+  });
   const [user, setUser] = useState('');
 
   const navigate = useNavigate();
   const location = useLocation();
 
+  const graphArray = [];
+  for (let i in metricStore) {
+    graphArray.push(
+      <LineGraph
+        metricStore={metricStore[i]}
+        graphTitle={i}
+        dataLabel={i}
+        timeLabels={metricStore[i]}
+      />
+    );
+  }
 
-  const { promAddress } = location.state ? location.state : { promAddress: null }
-  
+  const { promAddress } = location.state
+    ? location.state
+    : { promAddress: null };
 
   useLayoutEffect(() => {
-      async function verifyToken() {
-        const res = await fetch('/user/auth');
-        if (res.status === 401) {
-          return navigate('/login');
-        }
-        if (!res.ok) {
-          throw Error('failed to authenticate user')
-        }
-        const { username } = await res.json()
-        setUser(username)
+    async function verifyToken() {
+      const res = await fetch('/user/auth');
+      if (res.status === 401) {
+        return navigate('/login');
       }
-      verifyToken();
-    }, []);
+      if (!res.ok) {
+        throw Error('failed to authenticate user');
+      }
+      const { username } = await res.json();
+      setUser(username);
+    }
+    verifyToken();
+  }, []);
 
   useEffect(() => {
-    const endPoint = `/metrics/default?promAddress=${promAddress}` 
+    const endPoint = `/metrics/default?promAddress=${promAddress}`;
 
     const interval = setInterval(async () => {
-      const res = await fetch(endPoint)
+      const res = await fetch(endPoint);
       if (!res.ok) {
-        throw Error('failed to fetch at updating cluster')
+        throw Error('failed to fetch at updating cluster');
       }
       const metrics = await res.json();
-      const newStore = {
-        bytesIn: metrics.bytesIn[1],
-        bytesOut: metrics.bytesIn[1],
-        cpuUsage: metrics.cpuUsage[1],
-        brokerCount: metrics.brokerCount[1],
-      }
-      setMetricStore({...metricStore, ...newStore})
-    }, 3000)
-    
-    return () => clearInterval(interval)
+      const newStore = { ...metricStore };
 
-  }, [metricStore])
+      newStore.bytesIn = newStore.bytesIn.slice(1);
+      newStore.bytesIn.push(metrics.bytesIn);
+      newStore.bytesOut = newStore.bytesOut.slice(1);
+      newStore.bytesOut.push(metrics.bytesOut);
+      newStore.cpuUsage = newStore.cpuUsage.slice(1);
+      newStore.cpuUsage.push(metrics.cpuUsage);
 
-  const graphTitle = 'Throughput'
+      setMetricStore(newStore);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [metricStore]);
+
+  const graphTitle = 'Throughput';
   return (
     <main className='fixed inset-0 flex flex-col bg-slate-300 border-slate-500 border-2 rounded-lg'>
       <nav className='bg-white p-4 flex space-x-4 rounded-lg my-2 justify-between'>
         <div>Prometheus Port: {promAddress}</div>
         <div>Hello, {user}!</div>
       </nav>
-      <div className='bg-white flex-grow flex-col flex items-center justify-center rounded-lg'>
-        <div><h1>Active Broker Count: {metricStore.brokerCount}</h1></div>
-        <div><LineGraph metricStore={metricStore} graphTitle={graphTitle} /></div>
+      <div className='overflow-y-auto bg-white items-center justify-center rounded-lg'>
+        <div>
+          {/* <h1>Active Broker Count: {metricStore.brokerCount}</h1> */}
+        </div>
+        {graphArray}
       </div>
     </main>
   );
--- src/component/LineGraph.jsx
+++ src/component/LineGraph.jsx
@@ -3,17 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Line } from 'react-chartjs-2';
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    Colors
-  } from 'chart.js';
-  
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Colors,
+} from 'chart.js';
 
 ChartJS.register(
   CategoryScale,
@@ -26,43 +25,50 @@ ChartJS.register(
   Colors
 );
 
+export default function LineGraph({
+  graphTitle,
+  metricStore,
+  timeLabels,
+  dataLabel,
+}) {
+  const labels = [];
+  for (let i = 0; i < metricStore.length; i++) {
+    let time = new Date(metricStore[i][0] * 1000);
+    time = time.toTimeString().slice(0, 8);
+    labels.push(time);
+    // console.log('label time: ', metric[i][0]);
+  }
+  console.log(labels);
 
-
-export default function LineGraph({graphTitle, metricStore, timeLabels }) {
-  const labels = ['14:00', '14:05', '14:10', '14:15'];
-  const {bytesIn, bytesOut, cpuUsage} = metricStore;
+  const dataArr = [];
+  for (let i = 0; i < metricStore.length; i++) {
+    dataArr.push(metricStore[i][1]);
+  }
+  // console.log(dataArr);
 
   const options = {
     responsive: true,
     plugins: {
       colors: { forceOverride: true },
       legend: { position: 'top' },
-      title: { 
+      title: {
         display: true,
         text: graphTitle,
       },
     },
   };
-  
+
   const data = {
     labels,
     datasets: [
       {
-        label: 'Bytes In',
-        data: labels.map(() => bytesIn),
+        label: dataLabel,
+        data: dataArr,
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
-      {
-        label: 'Bytes Out',
-        data: labels.map(() => bytesOut),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
     ],
   };
-  
-  return (
-    <Line options={options} data={data} />
-  )
+
+  return <Line className='h-12 w-16' options={options} data={data} />;
 }
