1744741e159bdde4227d8f47a64cfa383acfc21f
conniedonahue <connordonahue09@gmail.com>
2023-11-11 11:17:35 -0500

    finished testing confirm password
    Co-authored-by: Wilson <jwu8475>
    Co-authored-by: David <dtezz>

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Homepage from './Routes/Homepage.jsx'
-import Login from './Routes/login.jsx'
-import Dashboard from './Routes/Dashboard.jsx'
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Homepage from './containers/Homepage.jsx';
+import Login from './containers/login.jsx';
+import Dashboard from './containers/Dashboard.jsx';
 
 function App() {
-
   return (
-      <Routes>
-        <Route path='/' element={<Homepage />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/dashboard' element={<Dashboard />}/>
-      </Routes>
-  )
+    <Routes>
+      <Route path='/' element={<Homepage />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
