efc7c49ae0e008183c5994e8248c030dc068ff64
Dominic Kenny <dominicjkenny@gmail.com>
2024-01-20 19:44:28 -0500

    clusterfunk -> klusterfunk

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'ClusterFunk',
+    dbName: 'KlusterFunk',
   })
   .then(() => console.log('Connected to Mongo DB'))
   .catch((err) => console.log(err));
